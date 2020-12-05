82d3d391c99fd8bd4273b5ef177751af4d348ba2
Tummetott <jonas.panizza@gmail.com>
2020-12-05 11:01:39 +0000

    finished code for assignment 4b

--- ex2-hash-dynamic/develop/hashing/main.cpp
+++ ex2-hash-dynamic/develop/hashing/main.cpp
@@ -8,7 +8,7 @@
 #include <unordered_map>
 #include <vector>
 
-int num_insertions = 1 << 26;
+int num_insertions = 1u << 26u;
 
 // This hash table uses linear probing.
 struct static_table {
@@ -83,6 +83,205 @@ struct static_table {
 	cell *cells = nullptr;
 };
 
+// This hash table uses linear probing, uses the modulo strategy to determine a cell and resizes dynamicly
+struct dynamic_modulo_table {
+	static constexpr const char *name = "dynamic_modulo";
+
+	struct cell {
+		int key;
+		int value;
+		bool valid = false;
+	};
+
+    size_t table_size = 8;
+    size_t insertions = 0;
+	cell *cells = nullptr;
+    float max_fill;
+
+    // Initidalize our array with 8 cells
+    dynamic_modulo_table(float max_fill) : cells{new cell[table_size]{}} {
+        this->max_fill = max_fill;
+    }
+
+	// Note: to simply the implementation, destructors and copy/move constructors are missing.
+
+	// This function turns a hash into an index in [0, m).
+	// This computes h % m but since m is a power of 2,
+	// we can compute the modulo by using a bitwise AND to cut off the leading binary digits.
+	// As hash, we just use the integer key directly.
+	int hash_to_index(int h) {
+		return h & (table_size - 1);
+	}
+
+	void put(int k, int v) {
+		int i = 0;
+
+		while(true) {
+			assert(i < table_size);
+
+			auto idx = hash_to_index(k + i);
+			auto &c = cells[idx];
+
+			if(!c.valid) {
+				c.key = k;
+				c.value = v;
+				c.valid = true;
+                ++insertions;
+                if((float)insertions / table_size > max_fill) {
+                    resize_table();
+                }
+				return;
+			}
+
+			if(c.key == k) {
+				c.value = v;
+				return;
+			}
+
+			++i;
+		}
+	}
+
+	std::optional<int> get(int k) {
+		int i = 0;
+
+		while(true) {
+			assert(i < table_size);
+
+			auto idx = hash_to_index(k + i);
+			auto &c = cells[idx];
+
+			if(!c.valid)
+				return std::nullopt;
+
+			if(c.key == k)
+				return c.value;
+
+			++i;
+		}
+	}
+
+    // We must rehash all our entries in order to resize the hashtable
+    void resize_table() {
+        // Save old state
+        cell * old_cells = cells;
+        auto old_size = table_size;
+
+        // Double the size
+        table_size = table_size << 1u;
+        cells = new cell[table_size];
+
+        // Rehash existing entries in new table
+        for(size_t i = 0; i < old_size; ++i) {
+			auto &c = old_cells[i];
+            if(c.valid) {
+                this->put(c.key, c.value);
+            }
+        }
+        delete [] old_cells;
+    }
+};
+
+// This hash table uses linear probing, uses the scaling strategy to determine a cell and resizes dynamicly
+struct dynamic_scaling_table {
+	static constexpr const char *name = "dynamic_scaling";
+
+	struct cell {
+		int key;
+		int value;
+		bool valid = false;
+	};
+
+    unsigned int exp = 3;
+    size_t insertions = 0;
+	cell *cells = nullptr;
+    float max_fill;
+
+    // Initidalize our array with 8 cells
+    dynamic_scaling_table(float max_fill) : cells{new cell[8]{}} {
+        this->max_fill = max_fill;
+    }
+
+	// Note: to simply the implementation, destructors and copy/move constructors are missing.
+
+    // This function computes h * m / 2 ^ 31. Since m = 2 ^ exp, we can do everything as bitshift
+	// As hash, we just use the integer key directly.
+    int scaling(int h) {
+        return (h << exp) >> 31u;
+    }
+
+	void put(int k, int v) {
+		int i = 0;
+
+		while(true) {
+            // assert i < table_size
+			assert(i < (1u << exp));
+
+			auto idx = scaling(k) + i;
+			auto &c = cells[idx];
+
+			if(!c.valid) {
+				c.key = k;
+				c.value = v;
+				c.valid = true;
+                ++insertions;
+                // if fill factor > max fill factor
+                if((insertions >> exp) > max_fill) {
+                    resize_table();
+                }
+				return;
+			}
+
+			if(c.key == k) {
+				c.value = v;
+				return;
+			}
+
+			++i;
+		}
+	}
+
+	std::optional<int> get(int k) {
+		int i = 0;
+
+		while(true) {
+            // assert i < table_size
+			assert(i < (1u << exp));
+
+			auto idx = scaling(k) + i;
+			auto &c = cells[idx];
+
+			if(!c.valid)
+				return std::nullopt;
+
+			if(c.key == k)
+				return c.value;
+
+			++i;
+		}
+	}
+
+    // We must rehash all our entries in order to resize the hashtable
+    void resize_table() {
+        // Save old state
+        cell * old_cells = cells;
+        auto old_size = 1u << exp;
+
+        // Double the size
+        ++exp;
+        cells = new cell[1u << exp];
+
+        // Rehash existing entries in new table
+        for(size_t i = 0; i < old_size; ++i) {
+			auto &c = old_cells[i];
+            if(c.valid) {
+                this->put(c.key, c.value);
+            }
+        }
+        delete [] old_cells;
+    }
+};
+
 // For comparsion, an implementation that uses std::unordered_map.
 // This is apples-to-oranges since std::unordered_map does not respect our fill factor.
 struct stl_table {
@@ -134,7 +333,7 @@ static const char *usage_text =
 	"Usage: hashing [OPTIONS]\n"
 	"Possible OPTIONS are:\n"
 	"    --algo ALGORITHM\n"
-	"        Select an algorithm {static,stl}.\n"
+	"        Select an algorithm {static,dynamic_modulo,dynamic_scaling,stl}.\n"
 	"    --max-fill FACTOR\n"
 	"        Set the maximal fill factor before the table grows.\n";
 
@@ -186,6 +385,10 @@ int main(int argc, char **argv) {
 
 	if(algorithm == "static") {
 		microbenchmark<static_table>(max_fill);
+	}else if(algorithm == "dynamic_modulo") {
+		microbenchmark<dynamic_modulo_table>(max_fill);
+	}else if(algorithm == "dynamic_scaling") {
+		microbenchmark<dynamic_scaling_table>(max_fill);
 	}else if(algorithm == "stl") {
 		microbenchmark<stl_table>(max_fill);
 	}else{
